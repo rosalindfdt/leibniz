346ed43b5100d74959ef7b5ebcc4a86e912bd09b
Scout Quinn <scout@dfdt.me>
2019-03-08 14:15:38 +1100

    commit more scout

--- eval.js
+++ eval.js
@@ -1,9 +1,100 @@
+var Functions = /** @class */ (function () {
+    function Functions() {
+    }
+    Functions.prototype.assign = function (func) {
+        switch (func.func[0]) {
+            case "a": {
+                this.a = func;
+            }
+            case "b": {
+                this.b = func;
+            }
+            case "c": {
+                this.c = func;
+            }
+            case "d": {
+                this.d = func;
+            }
+            case "e": {
+                this.e = func;
+            }
+            case "f": {
+                this.f = func;
+            }
+            case "g": {
+                this.g = func;
+            }
+            case "h": {
+                this.h = func;
+            }
+            case "i": {
+                this.i = func;
+            }
+            case "j": {
+                this.j = func;
+            }
+            case "k": {
+                this.k = func;
+            }
+            case "l": {
+                this.l = func;
+            }
+            case "m": {
+                this.m = func;
+            }
+            case "n": {
+                this.n = func;
+            }
+            case "o": {
+                this.o = func;
+            }
+            case "p": {
+                this.p = func;
+            }
+            case "q": {
+                this.q = func;
+            }
+            case "r": {
+                this.r = func;
+            }
+            case "s": {
+                this.s = func;
+            }
+            case "t": {
+                this.t = func;
+            }
+            case "u": {
+                this.u = func;
+            }
+            case "v": {
+                this.v = func;
+            }
+            case "w": {
+                this.w = func;
+            }
+            case "x": {
+                this.x = func;
+            }
+            case "y": {
+                this.y = func;
+            }
+            case "z": {
+                this.z = func;
+            }
+        }
+    };
+    return Functions;
+}());
 var Polynomial = /** @class */ (function () {
     function Polynomial(func) {
         this.func = func;
     }
     Polynomial.prototype.stripWhitespace = function (f) {
         return f.replace(/\s/g, "");
+    };
+    Polynomial.prototype.parse = function () {
+        var f = this.func.match(/^[a-z](?=:=)\([a-z]\)/);
+        return f;
     };
     Polynomial.prototype.evaluate = function () {
         var f = this.stripWhitespace(this.func);
@@ -13,11 +104,42 @@ var Polynomial = /** @class */ (function () {
                 g[term] = "+" + g[term];
             }
         }
+        var h = [];
+        for (var _i = 0, g_1 = g; _i < g_1.length; _i++) {
+            var item = g_1[_i];
+            var coefficient = item.match(/(\+|-)[0-9][0-9]*/g);
+            var variable = item.match(/[a-z]/g)[0];
+            var exponent = item.match(/(?<=\^)[0-9][0-9]*/g);
+            if (coefficient !== null) {
+                coefficient = Number(coefficient[0]);
+            }
+            else {
+                coefficient = 1;
+            }
+            if (exponent !== null) {
+                exponent = Number(exponent[0]);
+            }
+            else {
+                exponent = 1;
+            }
+            h.push({ coefficient: coefficient, variable: variable, exponent: exponent });
+        }
         var c = f.match(/(\+|-)[0-9][0-9]*(?![a-z])/g);
-        return [g, c];
+        var k = [];
+        for (var item in c) {
+            k[item] = Number(c[item]);
+        }
+        var constant = 0;
+        for (var _a = 0, k_1 = k; _a < k_1.length; _a++) {
+            var item = k_1[_a];
+            constant = constant + item;
+        }
+        var cterm = { coefficient: constant, variable: "", exponent: 0 };
+        h.push(cterm);
+        return h;
     };
     return Polynomial;
 }());
 var x = { coefficient: 1, variable: "x", exponent: 2 };
-var f = new Polynomial("x^2 - 2x + 1");
+var f = new Polynomial("x^2 - 2x + 1 - 2");
 console.log(f.evaluate());
--- eval.ts
+++ eval.ts
@@ -4,6 +4,66 @@ interface Term {
     exponent: number;
 }
 
+class Functions {
+    a?: Polynomial;
+    b?: Polynomial;
+    c?: Polynomial;
+    d?: Polynomial;
+    e?: Polynomial;
+    f?: Polynomial;
+    g?: Polynomial;
+    h?: Polynomial;
+    i?: Polynomial;
+    j?: Polynomial;
+    k?: Polynomial;
+    l?: Polynomial;
+    m?: Polynomial;
+    n?: Polynomial;
+    o?: Polynomial;
+    p?: Polynomial;
+    q?: Polynomial;
+    r?: Polynomial;
+    s?: Polynomial;
+    t?: Polynomial;
+    u?: Polynomial;
+    v?: Polynomial;
+    w?: Polynomial;
+    x?: Polynomial;
+    y?: Polynomial;
+    z?: Polynomial;
+
+    assign(func: Polynomial) {
+        switch(func.func[0]) {
+            case "a": { this.a = func; }
+            case "b": { this.b = func; }
+            case "c": { this.c = func; }
+            case "d": { this.d = func; }
+            case "e": { this.e = func; }
+            case "f": { this.f = func; }
+            case "g": { this.g = func; }
+            case "h": { this.h = func; }
+            case "i": { this.i = func; }
+            case "j": { this.j = func; }
+            case "k": { this.k = func; }
+            case "l": { this.l = func; }
+            case "m": { this.m = func; }
+            case "n": { this.n = func; }
+            case "o": { this.o = func; }
+            case "p": { this.p = func; }
+            case "q": { this.q = func; }
+            case "r": { this.r = func; }
+            case "s": { this.s = func; }
+            case "t": { this.t = func; }
+            case "u": { this.u = func; }
+            case "v": { this.v = func; }
+            case "w": { this.w = func; }
+            case "x": { this.x = func; }
+            case "y": { this.y = func; }
+            case "z": { this.z = func; }
+        }
+    }
+}
+
 class Polynomial {
     func: string;
     polynomial: Array<Term>;
@@ -16,6 +76,11 @@ class Polynomial {
         return f.replace(/\s/g, "");
     }
 
+    parse() {
+        let f: Array<string> = this.func.match(/^[a-z](?=:=)\([a-z]\)/);
+        return f;
+    }
+
     evaluate() {
         let f: string = this.stripWhitespace(this.func)
         let g: Array<string> = f.match(/(\+|-)?(([0-9][0-9]*)?[a-z](\^[0-9][0-9]*)?)/g)
@@ -25,15 +90,48 @@ class Polynomial {
             }
         }
 
-        let c: Array<string> = f.match(/(\+|-)[0-9][0-9]*(?![a-z])/g)
-        return [g, c]
+        let h: Array<Term> = [];
+        for (let item of g) {
+            let coefficient: number | RegExpMatchArray = item.match(/(\+|-)[0-9][0-9]*/g);
+            let variable: string = item.match(/[a-z]/g)[0];
+            let exponent: number | RegExpMatchArray = item.match(/(?<=\^)[0-9][0-9]*/g)
 
+            if (coefficient !== null) {
+                coefficient = Number(coefficient[0]);
+            } else {
+                coefficient = 1
+            }
+
+            if (exponent !== null) {
+                exponent = Number(exponent[0]);
+            } else {
+                exponent = 1;
+            }
+
+            h.push({coefficient: coefficient, variable: variable, exponent: exponent});
+        }
+
+        let c: Array<string> = f.match(/(\+|-)[0-9][0-9]*(?![a-z])/g)
+        let k: Array<number> = [];
+        for (let item in c) {
+            k[item] = Number(c[item]);
+        }
+
+        let constant: number = 0;
+        for (let item of k) {
+            constant = constant + item;
+        }
+
+        let cterm: Term = {coefficient: constant, variable: "",  exponent: 0};
+
+        h.push(cterm);
+
+        return h;
 
     }
 }
 
 let x = {coefficient: 1, variable: "x", exponent: 2};
-let f = new Polynomial("x^2 - 2x + 1");
+let f = new Polynomial("x^2 - 2x + 1 - 2");
 
 console.log(f.evaluate());
-
